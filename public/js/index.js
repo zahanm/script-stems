69f650b133ea23fe111b32f01c3c3633b9691f04
Zahan Malkani <zahanm@gmail.com>
2012-11-11 01:43:46 -0800

    working static tree layout

--- public/js/index.js
+++ public/js/index.js
@@ -2,23 +2,42 @@
 (function () {
 
   var width = 960;
-  var height = 600;
+  var height = 700;
 
-  var canvas = d3.select("#scripttree")
-  .append("svg")
+  var canvas = d3.select("#scripttree").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
-    .attr("transform", "translate(32, " + (height / 2) + ")");
+    .attr("transform", "translate(" + 30 + "," + 20 + ")");
 
-    var text = canvas.selectAll("text")
-    .data([10, 20, 30])
+  var diagonal = d3.svg.diagonal();
 
-    text.enter()
-    .append("text")
-      .attr("class", "enter")
-      .attr("x", function (d, i) { return d*10; })
-      .attr("y", 0)
-      .text(function (d) { return d; });
+  var tree = d3.layout.tree()
+    .size([width - 100, 525]);
+
+  d3.json("/data/scripts.json", function (json) {
+    var nodes = tree.nodes(json);
+
+    var link = canvas.selectAll("path.link")
+      .data(tree.links(nodes))
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", diagonal);
+
+    var node = canvas.selectAll("g.node")
+      .data(nodes)
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function (d) { console.log(d); return "translate(" + d.x + "," + d.y + ")"; });
+
+    node.append("circle")
+      .attr("r", 10);
+
+    node.append("text")
+      .attr("dx", function (d) { return - d.title.length * 3; })
+      .attr("dy", 30)
+      .text(function (d) { return d.title; });
+
+  });
 
 }());
