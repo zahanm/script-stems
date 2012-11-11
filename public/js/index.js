c587b9635e05507cbb8583b0ac5ce362a5100993
Zahan Malkani <zahanm@gmail.com>
2012-11-10 19:03:45 -0800

    placeholder simple example in d3 svg

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,24 @@
 
 (function () {
 
-  console.log("Hello to you too");
+  var width = 960;
+  var height = 600;
+
+  var canvas = d3.select("#scripttree")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(32, " + (height / 2) + ")");
+
+    var text = canvas.selectAll("text")
+    .data([10, 20, 30])
+
+    text.enter()
+    .append("text")
+      .attr("class", "enter")
+      .attr("x", function (d, i) { return d*10; })
+      .attr("y", 0)
+      .text(function (d) { return d; });
 
 }());
